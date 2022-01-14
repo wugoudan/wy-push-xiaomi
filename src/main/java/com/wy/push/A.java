3a265eaca9319b944d568e4a23c6f2d333b13ad2
wugoudan <wulog@outlook.com>
2022-01-14 15:31:44 +0800

    1.0.0

---
+++ src/main/java/com/wy/push/A.java
@@ -0,0 +1,13 @@
+package com.wy.push;
+
+/**
+ * @author wugoudan wulog@outlook.com
+ * @constructor
+ * @date 2022/1/14 15:31
+ * @effect
+ */
+public class A {
+    public static void aa(){
+
+    }
+}
